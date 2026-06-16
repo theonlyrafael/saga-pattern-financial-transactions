85c9e4d99cff8bc36a28c85428952adb5d68a7cd
Rafael Fraga <rafalfraga@gmail.com>
2026-06-16 03:06:46 -0300

    feat: cria modelo de dominio para ordem de compra

---
+++ conta-service/src/main/java/com/example/conta_service/domain/OrdemCompra.java
@@ -0,0 +1,47 @@
+package com.example.conta_service.domain;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class OrdemCompra {
+
+    private String id;
+    private String cpfCliente;
+    private BigDecimal valor;
+
+    // construtor vazio (obrigatório para o spring transformar em json depois)
+    public OrdemCompra() {
+    }
+
+    // construtor com dados
+    public OrdemCompra(String cpfCliente, BigDecimal valor) {
+        this.id = UUID.randomUUID().toString(); // Gera um ID único aleatório
+        this.cpfCliente = cpfCliente;
+        this.valor = valor;
+    }
+
+    // getters e setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCpfCliente() {
+        return cpfCliente;
+    }
+
+    public void setCpfCliente(String cpfCliente) {
+        this.cpfCliente = cpfCliente;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+}
