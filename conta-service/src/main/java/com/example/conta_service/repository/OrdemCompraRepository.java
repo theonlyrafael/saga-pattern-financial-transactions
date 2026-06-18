d32dee358eda8dd3073143f943fcbdb3226f6983
Rafael Fraga <rafalfraga@gmail.com>
2026-06-18 16:02:45 -0300

    refactor: remove anotacao repository e importacao redundantes

--- conta-service/src/main/java/com/example/conta_service/repository/OrdemCompraRepository.java
+++ conta-service/src/main/java/com/example/conta_service/repository/OrdemCompraRepository.java
@@ -2,12 +2,10 @@ package com.example.conta_service.repository;
 
 import com.example.conta_service.domain.OrdemCompra;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 // avisa ao spring que essa interface é um repositório, ou seja, uma classe que vai acessar o banco de dados
-@Repository
 public interface OrdemCompraRepository extends JpaRepository<OrdemCompra, UUID> {
 
 }
