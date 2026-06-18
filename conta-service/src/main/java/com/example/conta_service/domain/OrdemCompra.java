44536d7dbff784fbd81da264be639c923fafde42
Rafael Fraga <rafalfraga@gmail.com>
2026-06-18 03:47:11 -0300

    feat: mapeia classe ordemcompra como entidade do banco h2
    Código antigo foi preservado e está comentado embaixo do código atual e atualizado da classe OrdemCompra.java.

--- conta-service/src/main/java/com/example/conta_service/domain/OrdemCompra.java
+++ conta-service/src/main/java/com/example/conta_service/domain/OrdemCompra.java
@@ -1,5 +1,78 @@
 package com.example.conta_service.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+// avisa o framework que esta classe deve virar uma tabela no banco de dados
+@Entity
+public class OrdemCompra {
+
+    // define este campo como a chave primária da tabela
+    @Id
+    // manda o banco de dados gerar o uuid automaticamente na hora de salvar
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String cpfCliente;
+    private BigDecimal valor;
+    private String status;
+
+    // construtor vazio (obrigatório para o spring transformar em json e para o mapeamento do jpa)
+    public OrdemCompra() {
+    }
+
+    // construtor com dados para ser instanciado na criação de novas ordens
+    public OrdemCompra(String cpfCliente, BigDecimal valor) {
+        this.cpfCliente = cpfCliente;
+        this.valor = valor;
+        this.status = "PENDENTE"; // toda ordem nasce com este status
+    }
+
+    // getters e setters
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getCpfCliente() {
+        return cpfCliente;
+    }
+
+    public void setCpfCliente(String cpfCliente) {
+        this.cpfCliente = cpfCliente;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
+
+
+
+// ------ CÓDIGO ANTIGO ------
+
+/* 
+package com.example.conta_service.domain;
+
 import java.math.BigDecimal;
 import java.util.UUID;
 
@@ -45,3 +118,4 @@ public class OrdemCompra {
         this.valor = valor;
     }
 }
+    */
