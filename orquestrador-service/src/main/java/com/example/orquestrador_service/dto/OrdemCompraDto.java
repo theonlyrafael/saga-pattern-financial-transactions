97dd8473482d61179cc76187b0b40e0d631527fb
Rafael Fraga <rafalfraga@gmail.com>
2026-06-27 01:44:19 -0300

    feat: cria dto OrdemCompraDto para recepcao de ordens vindas do conta-service

---
+++ orquestrador-service/src/main/java/com/example/orquestrador_service/dto/OrdemCompraDto.java
@@ -0,0 +1,44 @@
+package com.example.orquestrador_service.dto;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class OrdemCompraDto {
+
+    private UUID id;
+    private String cpfCliente;
+    private BigDecimal valor;
+
+    public OrdemCompraDto() {
+    }
+
+    public OrdemCompraDto(UUID id, String cpfCliente, BigDecimal valor) {
+        this.id = id;
+        this.cpfCliente = cpfCliente;
+        this.valor = valor;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getCpfCliente() {
+        return cpfCliente;
+    }
+
+    public void setCpfCliente(String cpfCliente) {
+        this.cpfCliente = cpfCliente;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+}
